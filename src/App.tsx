1609ea6e48bc85b7af1386b59aac9e242adc2fa3
kimura.coki <kimura.coki@gmail.com>
2025-04-14 22:34:11 +0900

    Add getting profile

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,25 @@ import "./App.css";
 function App() {
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
+  const [name, setName] = useState("");
+  const [picture, setPicture] = useState("");
 
   useEffect(() => {
     liff
       .init({
-        liffId: import.meta.env.VITE_LIFF_ID
+        liffId: import.meta.env.VITE_LIFF_ID,
       })
       .then(() => {
         setMessage("LIFF init succeeded.");
+        liff
+          .getProfile()
+          .then((profile) => {
+            setName(profile.displayName);
+            setPicture(profile.pictureUrl ? profile.pictureUrl : "");
+          })
+          .catch((err) => {
+            console.log("error", err);
+          });
       })
       .catch((e: Error) => {
         setMessage("LIFF init failed.");
@@ -29,6 +40,8 @@ function App() {
           <code>{error}</code>
         </p>
       )}
+      {name && <p>こんにちは、{name}さん</p>}
+      {picture && <img src={picture}></img>}
       <a
         href="https://developers.line.biz/ja/docs/liff/"
         target="_blank"
